5e919645d3bfda093fdc8bfffad6090b9903556b
Lobod <konta.lobod@o2.pl>
2018-12-28 12:23:01 +0100

    Feixed common issues

--- app/MenuHandler.js
+++ app/MenuHandler.js
@@ -1,41 +1,7 @@
-const $ = require('jquery');
-const { remote } =require('electron');
-var ipc = require('electron').ipcRenderer;
+const $ = require("jquery");
+const { remote } =require("electron");
+var ipc = require("electron").ipcRenderer;
 var win = remote.getCurrentWindow();
-$("#close").click(function(){
-    win.close();
-})
-$("#min").click(function(){
-    win.minimize();
-})
-ipc.on('location-reply', function (event, arg) {
-    var loc = JSON.parse(arg);
-        $(".location")[loc.id].innerText = loc.loc;
-  })
-ipc.on('update-reply', function (event, arg) {
-    update(arg);
-    event.sender.send("test","");
-    event.sender.send("lb",document.body.clientHeight);
-    setTimeout(() => {
-        update(arg);
-    }, 200);
-  })
-  ipc.on('pos-reply', function (event, arg) {
-    event.sender.send("ltest","");
-  })
-  ipc.on('wing-reply', function (event, arg) {
-    $(arg[0])[0].src="./app/icon_"+arg[1]+"_"+arg[2]+".svg";
-  })
-  ipc.on('wing-name', function (event, arg) {
-    $(arg[0])[0].title=arg[1];
-  })
-  ipc.on('wing-status', function (event, arg) {
-      if(arg[1]=="on")$(arg[0])[0].style.display="inline";
-      else $(arg[0])[0].style.display="none";
-  })
-  ipc.on('pos-reply', function (event, arg) {
-    event.sender.send("ltest","");
-  })
 function update(arg){
     var html = "";
     var elements = ""
@@ -48,8 +14,42 @@ function update(arg){
     }
     else var html=`<b>No data to display</b>`;
 
-    $('.card-title')[0].innerHTML = html;
+    $(".card-title")[0].html(html);
 }
+$("#close").click(function(){
+    win.close();
+})
+$("#min").click(function(){
+    win.minimize();
+})
+ipc.on("location-reply", function (event, arg) {
+    var loc = JSON.parse(arg);
+        $(".location")[loc.id].innerText = loc.loc;
+  })
+ipc.on("update-reply", function (event, arg) {
+    update(arg);
+    event.sender.send("test","");
+    event.sender.send("lb",document.body.clientHeight);
+    setTimeout(() => {
+        update(arg);
+    }, 200);
+  })
+  ipc.on("pos-reply", function (event, arg) {
+    event.sender.send("ltest","");
+  })
+  ipc.on("wing-reply", function (event, arg) {
+    $(arg[0])[0].src="./app/icon_"+arg[1]+"_"+arg[2]+".svg";
+  })
+  ipc.on("wing-name", function (event, arg) {
+    $(arg[0])[0].title=arg[1];
+  })
+  ipc.on("wing-status", function (event, arg) {
+      if(arg[1]=="on")$(arg[0])[0].style.display="inline";
+      else $(arg[0])[0].style.display="none";
+  })
+  ipc.on("pos-reply", function (event, arg) {
+    event.sender.send("ltest","");
+  })
 $(document).ready(()=>{
     ipc.send("ready!","");
 });
